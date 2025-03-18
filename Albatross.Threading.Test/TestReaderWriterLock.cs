347bffe0f1e3375ad7d705463a2ee71b889ec7fb
rushuiguan <rushui@outlook.com>
2025-03-18 10:46:13 -0400

    add async lock convenient methods back

---
+++ Albatross.Threading.Test/TestReaderWriterLock.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading;
+using Xunit;
+
+namespace Albatross.Threading.Test {
+	public class TestReaderWriterLock {
+		[Fact]
+		public void TestNotUpgradableReaderLock() {
+			using var slimLock = new ReaderWriterLockSlim();
+			using (var readerLock = new ReaderLock(slimLock, false)) {
+				Assert.True(slimLock.IsReadLockHeld);
+				Assert.False(slimLock.IsUpgradeableReadLockHeld);
+				Assert.False(slimLock.IsWriteLockHeld);
+			}
+			Assert.False(slimLock.IsReadLockHeld);
+		}
+
+		[Fact]
+		public void TestUpgradableReaderLock() {
+			using var slimLock = new ReaderWriterLockSlim();
+			using (var readerLock = new ReaderLock(slimLock, true)) {
+				Assert.False(slimLock.IsReadLockHeld);
+				Assert.True(slimLock.IsUpgradeableReadLockHeld);
+				Assert.False(slimLock.IsWriteLockHeld);
+			}
+			Assert.False(slimLock.IsUpgradeableReadLockHeld);
+		}
+
+		[Fact]
+		public void TestWriterLock() {
+			using var slimLock = new ReaderWriterLockSlim();
+			using (var readerLock = new WriterLock(slimLock)) {
+				Assert.False(slimLock.IsReadLockHeld);
+				Assert.False(slimLock.IsUpgradeableReadLockHeld);
+				Assert.True(slimLock.IsWriteLockHeld);
+			}
+			Assert.False(slimLock.IsWriteLockHeld);
+		}
+
+		[Fact]
+		public void TestAsyncLock() {
+			using SemaphoreSlim semaphore = new SemaphoreSlim(3);
+			using (new AsyncLock(semaphore)) {
+				Assert.Equal(2, semaphore.CurrentCount);
+				using (new AsyncLock(semaphore)) {
+					Assert.Equal(1, semaphore.CurrentCount);
+					using (new AsyncLock(semaphore)) {
+						Assert.Equal(0, semaphore.CurrentCount);
+					}
+				}
+			}
+
+			Assert.Equal(3, semaphore.CurrentCount);
+		}
+	}
+}
---
+++ Albatross.Threading/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+
+namespace Albatross.Threading {
+	public static class Extension {
+		public static ReaderLock GetReadLock(this ReaderWriterLockSlim readerWriterLock) => new ReaderLock(readerWriterLock, false);
+		public static ReaderLock GetUpgradeableReadLock(this ReaderWriterLockSlim readerWriterLock) => new ReaderLock(readerWriterLock, true);
+		public static WriterLock GetWriteLock(this ReaderWriterLockSlim readerWriterLock) => new WriterLock(readerWriterLock);
+		public static AsyncLock EnterAsyncLock(this SemaphoreSlim semaphore) {
+			return new AsyncLock(semaphore);
+		}
+	}
+}
